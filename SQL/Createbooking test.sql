88c7ca7bcefcddb4de50a6c986cbfd6fd1be00b4
briannanayakkara <dimalbrian@gmail.com>
2023-02-08 15:43:19 +0100

    Add one update  statment
    Update bookings SP

--- SQL/Createbooking test.sql
+++ SQL/Createbooking test.sql
@@ -1,7 +1,7 @@
-DECLARE	@username varchar(50) ='yaf'
-DECLARE	@VName varchar(50) = 'bar8'
-DECLARE	@datetime datetime ='2023-02-07 10:55'
-DECLARE	@Pax int =9
+DECLARE	@username varchar(50) ='brian'
+DECLARE	@VName varchar(50) = 'bar7'
+DECLARE	@datetime datetime ='2023-03-05 20:55'
+DECLARE	@Pax int =2
 DECLARE	@note varchar(max)='Gay OG'
 
 exec CreateBooking
@@ -9,9 +9,36 @@ exec CreateBooking
 	@VName,
 	@datetime,
 	@Pax,
-	@note
-	
-	
+	@note		
 
 	select * from Bookings
 
+	/*
+
+	DECLARE	@username varchar(50) ='brian'
+DECLARE	@VName varchar(50) = 'bar7'
+DECLARE	@datetime datetime ='2023-03-05 20:55'
+DECLARE	@Pax int =2
+DECLARE	@note varchar(max)='Gay OG'
+
+
+
+DECLARE @UserID int = (select UserID from Users where username = @username)
+
+	DECLARE @VenueID int= (select VenueID from Venues where Name = @VName)
+	DECLARE @AdminLvl int = (select AdminLevel from Users where username = @username)
+
+	DECLARE @tbID int = (select top 1 vi.TableID from VenueItems vi 
+							where vi.VenueID = @VenueID 
+									and vi.Pax >= @Pax 
+									and vi.TableID not in (select TableID from Bookings where cast(Time as date) >=  cast(@datetime as date))
+									--and vi.status not in (2,22)
+									order by vi.Pax) -- Not Available/booked or Arrived
+select @tbID
+select @VenueID
+select * from Bookings where UserID = @UserID and VenueID = @VenueID 
+					and cast(Time as datetime) >= cast(@datetime as datetime) and TableID = @tbID
+					
+					*/
+
+					
--- SQL/Look at booking script.sql
+++ SQL/Look at booking script.sql
@@ -1,18 +1,23 @@
 USE BookingSystem
 
-select v.Name,vi.TableID,vi.Pax,vi.Status from VenueItems vi join status s on vi.Status= s.ID	
+select b.ID,v.Name,vi.TableID,vi.Pax,vi.Status,s.Status,b.Time from VenueItems vi 
+						join status s on vi.Status= s.ID	
 						join Venues v on vi.VenueID = v.VenueID
 						join VenueOwners vo on vo.VenueID = v.VenueID
 						join Users u on vo.UserID = u.UserID
+						join Bookings b on v.VenueID = b.VenueID
+						where b.Time < getdate()-1
 						--join bookings b on b.VenueID = v.VenueID
 
 					
-select u.Firstname+' '+u.Lastname 'Full Name',u.Email,u.Phone,b.Pax 'Amount of people',vi.Pax 'Table size',b.Note,b.Time,v.name 'Venue' from bookings b 
+	select b.ID,u.Firstname+' '+u.Lastname 'Full Name',u.Email,u.Phone,b.Pax 'Amount of people',vi.Pax 'Table size',b.Note,b.Time,v.name 'Venue',uvi.Username 'Venue owner' from bookings b 
 							join users u on u.UserID = b.UserID 
 							join Venues v on b.VenueID = v.VenueID
 							join VenueItems vi on b.TableID = vi.TableID
+							join VenueOwners vo on v.VenueID = vo.VenueID
+							join Users uvi on vo.UserID = uvi.UserID
 							
-							
+				
 						
 
 						select * from Bookings
