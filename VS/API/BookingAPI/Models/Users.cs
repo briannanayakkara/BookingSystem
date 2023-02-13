02e52e33736c648b1a5090c0ced68d850452c53d
briannanayakkara <dimalbrian@gmail.com>
2023-02-13 17:11:49 +0100

    Setting up API users

---
+++ VS/API/BookingAPI/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿namespace BookingAPI.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FName { get; set; }
+        public string SName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Region { get; set; }
+        public string Birthday { get; set; }
+        public int AdminLevel { get; set; }
+
+
+
+
+
+    }
+}
